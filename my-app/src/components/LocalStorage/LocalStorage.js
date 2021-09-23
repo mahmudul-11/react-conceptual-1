522c8da17bc16ae2cce4d3cf373fe0285e6d3a85
Mahmudul Hasan <17101120@uap-bd.edu>
2021-09-23 13:27:36 +0600

    onlclick function call with pararmeter

---
+++ my-app/src/components/LocalStorage/LocalStorage.js
@@ -0,0 +1,6 @@
+import './LocalStorage.css'
+const addToLoacalStorage = (id) =>{
+    // localStorage.setItem(id);
+    console.log(id);
+}
+export{addToLoacalStorage}
--- my-app/src/components/ShowJson/ShowJson.js
+++ my-app/src/components/ShowJson/ShowJson.js
@@ -1,13 +1,17 @@
 import './ShowJson.css'
 import React from 'react';
+import { addToLoacalStorage } from '../LocalStorage/LocalStorage';
 
 const ShowJson = (props) => {
     // console.log(props.v);
     const {_id,name } = props.v
     return (
         <div className='cls'>
+            
             <h1>{name}</h1>
             <h3>{_id}</h3>
+            <button onClick={()=> addToLoacalStorage(_id)}>Purches</button> 
+            {/* Function with paramete  and for this an annonymus function is needed*/}
         </div>
     );
 };
